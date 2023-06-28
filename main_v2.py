4f634b209d27382627e6e2c2dcca0cfb4cef4424
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-06-28 12:41:32 +0530

    Style & Bias Choosing Option

--- main_v2.py
+++ main_v2.py
@@ -18,15 +18,33 @@ if __name__ == '__main__':
             current_sentence = word
     sentences.append(current_sentence.strip())
 
-    # Define the handwriting characteristics
-    biases = [.75 for _ in sentences]
-    styles = [9 for _ in sentences]
+    # Prompt the user for the bias and style behavior choice
+    print("Choose the bias and style behavior:")
+    print("1) Fixed bias and fixed style")
+    print("2) Variable bias and fixed style")
+    print("3) Fixed bias and variable style")
+    choice = input("Enter your choice (1/2/3): ")
+
+    # Define the handwriting characteristics based on the user's choice
+    biases = []
+    styles = []
     # stroke_colors = ['red', 'green', 'black', 'blue']
     # stroke_widths = [1, 2, 1, 2]
+    if choice == "1":
+        biases = [.95 for _ in sentences]
+        styles = [1 for _ in sentences]
+    elif choice == "2":
+        biases = [.75 for _ in sentences]
+        styles = np.cumsum(np.array([len(i) for i in sentences]) == 0).astype(int)
+    elif choice == "3":
+        biases = .2 * np.flip(np.cumsum([len(i) == 0 for i in sentences]), 0)
+        styles = [7 for _ in sentences]
+    else:
+        print("Invalid choice. Using default behavior.")
 
     # Generate the handwriting image
     hand.write(
-        filename='img/usage_demo.svg',
+        filename='img/usage_v2demo.svg',
         lines=sentences,
         biases=biases,
         styles=styles,
